df06f69ab807d8bcf358d44f1d92c25839fd08df
Jasynth <74651647+Jasynth@users.noreply.github.com>
2020-11-21 21:02:52 -0500

    Add files via upload

---
+++ PresetData.h
@@ -0,0 +1,28 @@
+/* (Auto-generated binary data file). */
+
+#ifndef BINARY_PRESETDATA_H
+#define BINARY_PRESETDATA_H
+
+namespace PresetData
+{
+    extern const char*  externalFiles;
+    const int           externalFilesSize = 51;
+
+    extern const char*  images;
+    const int           imagesSize = 3448005;
+
+    extern const char*  impulses;
+    const int           impulsesSize = 28;
+
+    extern const char*  preset;
+    const int           presetSize = 1144;
+
+    extern const char*  samplemaps;
+    const int           samplemapsSize = 28;
+
+    extern const char*  userPresets;
+    const int           userPresetsSize = 27;
+
+}
+
+#endif
